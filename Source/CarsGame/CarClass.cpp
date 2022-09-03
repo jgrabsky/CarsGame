af71ab594b805005c654db6a7e5eb8cb3fa83e14
John Grabsky <jgrabsky@hotmail.com>
2022-09-03 09:04:22 -0500

    57 capsule component

--- Source/CarsGame/CarClass.cpp
+++ Source/CarsGame/CarClass.cpp
@@ -11,6 +11,8 @@ ACarClass::ACarClass()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CarCapsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Car Capsule"));
+	RootComponent = CarCapsule;
 }
 
 // Called when the game starts or when spawned
